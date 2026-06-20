b607c24953dbdf1a1030654ae70f167396b55dbb
SwiftOperations <warehouse1@swiftsupply.ca>
2026-06-20 02:45:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.bootstrapStandalonePWA = function() {
   const pwaData = {
     "short_name": "StagingTracker", "name": "Swift Staging Tracker Hub",
     "icons": [{"src": "https://cdn-icons-png.flaticon.com/512/3014/3014166.png", "type": "image/png", "sizes": "512x512"}],
-    "start_url": ".", "background_color": "#f6f7f9", "theme_color": "#dd4d25", "display": "standalone", "orientation": "portrait"
+    "start_url": ".", "background_color": "#f1f5f9", "theme_color": "#dd4d25", "display": "standalone", "orientation": "portrait"
   };
   if($('#pwa-manifest')) $('#pwa-manifest').setAttribute('href', 'data:application/manifest+json;charset=utf-8,' + encodeURIComponent(JSON.stringify(pwaData)));
 };
